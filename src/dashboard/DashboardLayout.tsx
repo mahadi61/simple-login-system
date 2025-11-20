feef5b6e1303335273d592d0da02601a6ccc3f6e
Mahadi Hasan <mahadihasan8461@gmail.com>
2025-11-20 08:37:38 +0600

    dahboard layout desing done

--- src/dashboard/DashboardLayout.tsx
+++ src/dashboard/DashboardLayout.tsx
@@ -1,5 +1,47 @@
+import { Link, Outlet, useLocation } from "react-router";
+import Navbar from "../components/Navbar";
+
 const DashboardLayout = () => {
-  return <div></div>;
+  const location = useLocation();
+
+  return (
+    <div>
+      <Navbar />
+      <div className="flex h-screen">
+        {/* Left Sidebar */}
+        <div className="w-1/4 bg-gray-500 text-white p-5 flex flex-col">
+          <h2 className="text-2xl text-center font-bold mb-6">Dashboard</h2>
+
+          {/* Sidebar Buttons */}
+          <Link
+            to="/dashboard"
+            className={`mb-3 p-3 rounded-md text-center ${
+              location.pathname === "/dashboard"
+                ? "bg-blue-600"
+                : "bg-gray-700 hover:bg-gray-600"
+            }`}
+          >
+            All Users
+          </Link>
+          <Link
+            to="/dashboard/admin-users"
+            className={`mb-3 p-3 rounded-md text-center ${
+              location.pathname === "/dashboard/admin-users"
+                ? "bg-blue-600"
+                : "bg-gray-700 hover:bg-gray-600"
+            }`}
+          >
+            Admin Users
+          </Link>
+        </div>
+
+        {/* Right Content */}
+        <div className="w-3/4 p-6 bg-gray-100 overflow-auto">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router";
+import DashboardLayout from "../dashboard/DashboardLayout";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Registration from "../pages/Registration";
@@ -6,7 +7,13 @@ import Registration from "../pages/Registration";
 const Router = () => {
   return (
     <Routes>
+      {/* login and reg page */}
       <Route path="/" element={<Home />}>
+        <Route path="" element={<Login />} />
+        <Route path="registration" element={<Registration />} />
+      </Route>
+      {/* dashboard routes */}
+      <Route path="/dashboard" element={<DashboardLayout />}>
         <Route path="" element={<Login />} />
         <Route path="registration" element={<Registration />} />
       </Route>
